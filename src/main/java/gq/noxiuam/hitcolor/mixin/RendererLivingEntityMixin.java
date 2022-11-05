bc9d8fcdb8197ed84ba6629619264774a99efb9d
Tellinq <59412384+Tellinq@users.noreply.github.com>
2022-11-04 20:50:55 -0400

    Instead of overriding, use ModifyArg

--- src/main/java/gq/noxiuam/hitcolor/mixin/RendererLivingEntityMixin.java
+++ src/main/java/gq/noxiuam/hitcolor/mixin/RendererLivingEntityMixin.java
@@ -14,125 +14,77 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.nio.FloatBuffer;
 
 @Mixin(RendererLivingEntity.class)
 public abstract class RendererLivingEntityMixin {
+    private EntityLivingBase entitylivingbaseIn;
+    private float partialTicks;
 
-    @Shadow protected abstract int getColorMultiplier(EntityLivingBase entitylivingbaseIn, float lightBrightness, float partialTickTime);
+    @Inject(method = "setBrightness", at = @At("HEAD"))
+    private void setMethodVariables(EntityLivingBase entitylivingbaseIn, float partialTicks, boolean combineTextures, CallbackInfoReturnable<Boolean> cir) {
+        this.entitylivingbaseIn = entitylivingbaseIn;
+        this.partialTicks = partialTicks;
+    }
 
-    @Shadow protected FloatBuffer brightnessBuffer;
+    @ModifyArg(method = "setBrightness", at = @At(value = "INVOKE", target = "Ljava/nio/FloatBuffer;put(F)Ljava/nio/FloatBuffer;", ordinal = 0))
+    private float getRedTint(float red) {
+        HitColorConfig config = HitColor.INSTANCE.config;
+        HitColorConfig.HitColorConfiguration e = config.getEntityType(this.entitylivingbaseIn);
+        OneColor hitColorToUse = config.getColorToUse(e);
+        return this.getTheColor(red, hitColorToUse.getRed(), e);
+    }
 
-    @Shadow @Final private static DynamicTexture textureBrightness;
+    @ModifyArg(method = "setBrightness", at = @At(value = "INVOKE", target = "Ljava/nio/FloatBuffer;put(F)Ljava/nio/FloatBuffer;", ordinal = 1))
+    private float getGreenTint(float green) {
+        HitColorConfig config = HitColor.INSTANCE.config;
+        HitColorConfig.HitColorConfiguration entityType = config.getEntityType(this.entitylivingbaseIn);
+        OneColor hitColorToUse = config.getColorToUse(entityType);
+        return this.getTheColor(green, hitColorToUse.getGreen(), entityType);
+    }
 
-    /**
-     * Overrides the original brightness method.
-     */
-    @Inject(method = "setBrightness", at = @At(value = "HEAD"), cancellable = true)
-    public void injectSetBrightness(EntityLivingBase entitylivingbaseIn, float partialTicks, boolean combineTextures, CallbackInfoReturnable<Boolean> cir) {
-        float f = entitylivingbaseIn.getBrightness(partialTicks);
-        int i = this.getColorMultiplier(entitylivingbaseIn, f, partialTicks);
-        boolean flag = (i >> 24 & 255) > 0;
-        boolean flag1 = entitylivingbaseIn.hurtTime > 0 || entitylivingbaseIn.deathTime > 0;
+    @ModifyArg(method = "setBrightness", at = @At(value = "INVOKE", target = "Ljava/nio/FloatBuffer;put(F)Ljava/nio/FloatBuffer;", ordinal = 2))
+    private float getBlueTint(float blue) {
+        HitColorConfig config = HitColor.INSTANCE.config;
+        HitColorConfig.HitColorConfiguration entityType = config.getEntityType(this.entitylivingbaseIn);
+        OneColor hitColorToUse = config.getColorToUse(entityType);
+        return this.getTheColor(blue, hitColorToUse.getBlue(), entityType);
+    }
 
-        if (!flag && !flag1) {
-            cir.setReturnValue(false);
-        } else if (!flag && !combineTextures) {
-            cir.setReturnValue(false);
-        } else {
-            GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
-            GlStateManager.enableTexture2D();
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, OpenGlHelper.GL_COMBINE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_RGB, GL11.GL_MODULATE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.defaultTexUnit);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.GL_PRIMARY_COLOR);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND0_RGB, GL11.GL_SRC_COLOR);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND1_RGB, GL11.GL_SRC_COLOR);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_ALPHA, GL11.GL_REPLACE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.defaultTexUnit);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND0_ALPHA, GL11.GL_SRC_ALPHA);
-            GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
-            GlStateManager.enableTexture2D();
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, OpenGlHelper.GL_COMBINE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_RGB, OpenGlHelper.GL_INTERPOLATE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.GL_CONSTANT);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE2_RGB, OpenGlHelper.GL_CONSTANT);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND0_RGB, GL11.GL_SRC_COLOR);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND1_RGB, GL11.GL_SRC_COLOR);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND2_RGB, GL11.GL_SRC_ALPHA);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_ALPHA, GL11.GL_REPLACE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND0_ALPHA, GL11.GL_SRC_ALPHA);
-            this.brightnessBuffer.position(0);
-
-            float brightness = 1.0F;
-            HitColorConfig config = HitColor.INSTANCE.config;
-
-            HitColorConfig.HitColorConfiguration e = config.getEntityType(entitylivingbaseIn);
-
-            int animationTypeToUse = config.getAnimationTypeToUse(e);
-            boolean affectedByBrightnessToUse = config.getBrightnessToUse(e);
-            OneColor hitColorToUse = config.getColorToUse(e);
-
-            if (e == null)
-                return;
-
-            if (flag1) {
-                if (config.enabled) {
-                    f = affectedByBrightnessToUse ? f : 1.0F;
-                    float hitA = hitColorToUse.getAlpha() / 255F;
-                    if (animationTypeToUse != 0) {
-                        float percent = 1.0F - (float)entitylivingbaseIn.hurtTime / 10.0F;
-                        if (animationTypeToUse == 1) {
-                            percent = percent < 0.5F ? percent / 0.5F : (1.0F - percent) / 0.5F;
-                        } else if (animationTypeToUse == 2) {
-                            percent = (1.0F - percent);
-                        }
-
-                        hitA = hitA * percent;
-                    }
-
-                    this.brightnessBuffer.put((hitColorToUse.getRed() / 255F) * brightness * f);
-                    this.brightnessBuffer.put((hitColorToUse.getGreen() / 255F) * brightness * f);
-                    this.brightnessBuffer.put((hitColorToUse.getBlue() / 255F) * brightness * f);
-                    this.brightnessBuffer.put(hitA);
-                } else {
-                    this.brightnessBuffer.put(brightness);
-                    this.brightnessBuffer.put(0.0F);
-                    this.brightnessBuffer.put(0.0F);
-                    this.brightnessBuffer.put(0.3F);
+    @ModifyArg(method = "setBrightness", at = @At(value = "INVOKE", target = "Ljava/nio/FloatBuffer;put(F)Ljava/nio/FloatBuffer;", ordinal = 3))
+    private float getAlphaTint(float alpha) {
+        HitColorConfig config = HitColor.INSTANCE.config;
+        HitColorConfig.HitColorConfiguration entityType = config.getEntityType(this.entitylivingbaseIn);
+        if (config.enabled) {
+            int animationTypeToUse = config.getAnimationTypeToUse(entityType);
+            OneColor hitColorToUse = config.getColorToUse(entityType);
+            alpha = hitColorToUse.getAlpha() / 255F;
+            if (animationTypeToUse != 0) {
+                float percent = 1.0F - (float)this.entitylivingbaseIn.hurtTime / 10.0F;
+                if (animationTypeToUse == 1) {
+                    percent = percent < 0.5F ? percent / 0.5F : (1.0F - percent) / 0.5F;
+                } else if (animationTypeToUse == 2) {
+                    percent = (1.0F - percent);
                 }
-            } else {
-                float f1 = (float) (i >> 24 & 255) / 255.0F;
-                float f2 = (float) (i >> 16 & 255) / 255.0F;
-                float f3 = (float) (i >> 8 & 255) / 255.0F;
-                float f4 = (float) (i & 255) / 255.0F;
-                this.brightnessBuffer.put(f2);
-                this.brightnessBuffer.put(f3);
-                this.brightnessBuffer.put(f4);
-                this.brightnessBuffer.put(1.0F - f1);
-            }
 
-            this.brightnessBuffer.flip();
-            GL11.glTexEnv(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_COLOR, this.brightnessBuffer);
-            GlStateManager.setActiveTexture(OpenGlHelper.GL_TEXTURE2);
-            GlStateManager.enableTexture2D();
-            GlStateManager.bindTexture(textureBrightness.getGlTextureId());
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, OpenGlHelper.GL_COMBINE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_RGB, GL11.GL_MODULATE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.lightmapTexUnit);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND0_RGB, GL11.GL_SRC_COLOR);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND1_RGB, GL11.GL_SRC_COLOR);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_COMBINE_ALPHA, GL11.GL_REPLACE);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, OpenGlHelper.GL_OPERAND0_ALPHA, GL11.GL_SRC_ALPHA);
-            GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
-            cir.setReturnValue(true);
+                alpha = alpha * percent;
+            }
+            return alpha;
         }
+        return alpha;
+    }
+
+    private float getTheColor(float color, int specified, HitColorConfig.HitColorConfiguration entityType) {
+        HitColorConfig config = HitColor.INSTANCE.config;
+        if (config.enabled) {
+            boolean affectedByBrightnessToUse = config.getBrightnessToUse(entityType);
+            float brightness = affectedByBrightnessToUse ? this.entitylivingbaseIn.getBrightness(this.partialTicks) : 1.0F;
+            return (specified / 255F) * brightness;
+        }
+        return color;
     }
 
 }
